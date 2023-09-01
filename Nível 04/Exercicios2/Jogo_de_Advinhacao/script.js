031bbe242e2803ac93dc789831e13ba95c86cf97
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2023-09-01 14:57:48 -0300

    Update in exercise

--- Nível 04/Exercicios2/Jogo_de_Advinhacao/script.js
+++ Nível 04/Exercicios2/Jogo_de_Advinhacao/script.js
@@ -16,14 +16,16 @@
   Substitua o "x" da mensagem, pelo número de tentativas
 */
 
-let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
-const randomNumber = Math.round(Math.random() * 10)
+let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10");
+const randomNumber = Math.round(Math.random() * 10);
 
-let xAttempts = 1
+let xAttempts = 1;
 
-while(Number(result) != randomNumber) {
-  result = prompt("Erro, tente novamente:")
-  xAttempts++
+while (Number(result) != randomNumber) {
+  result = prompt("Erro, tente novamente:");
+  xAttempts++;
 }
 
-alert(`Parabéns! O número que eu pensei foi ${randomNumber} e você advinhou o número em ${xAttempts} tentativas`)
+alert(
+  `Parabéns! O número que eu pensei foi ${randomNumber} e você advinhou o número em ${xAttempts} tentativas`
+);
