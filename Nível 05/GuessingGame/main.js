2085bda7f2be3a7ff79fda1095a4bf793de5228c
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2023-10-04 14:49:21 -0300

    Bug fixes

--- Nível 05/GuessingGame/main.js
+++ Nível 05/GuessingGame/main.js
@@ -3,12 +3,13 @@ const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-const randomNumber = Math.floor(Math.random() * 10)
+let randomNumber = Math.floor(Math.random() * 10)
 let xAttempts = 1
 
 // Eventos
 btnTry.addEventListener("click", handleTryClick)
 btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", handleClickEnter)
 
 // Funções
 function handleTryClick(event) {
@@ -16,10 +17,21 @@ function handleTryClick(event) {
 
   const inputNumber = document.querySelector("#inputNumber")
 
+  if (inputNumber.value === "") {
+    alert("Informe um número!")
+    inputNumber.value = ""
+    xAttempts = 1
+    return
+  }
+
   if (Number(inputNumber.value) == randomNumber) {
     toggleScreen()
 
     screen2.querySelector("h2").innerText = `Você acertou em ${xAttempts} tentativas!`
+  } else if (Number(inputNumber.value) < 0) {
+    alert("O número digitado é menor do que zero")
+  } else if (Number(inputNumber.value) > 10) {
+    alert("O número digitado é maior que 10")
   }
 
   inputNumber.value = ""
@@ -29,9 +41,16 @@ function handleTryClick(event) {
 function handleResetClick(event) {
   toggleScreen()
   xAttempts = 1
+  randomNumber = Math.floor(Math.random() * 10)
 }
 
 function toggleScreen() {
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
+}
+
+function handleClickEnter(event) {
+  if (event.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick(event)
+  }
 }
